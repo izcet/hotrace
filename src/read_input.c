5bdef4ab106b3acca09c8fdfc007c0d7a7c1b590
Isaac Rhett <isaacjrhett@gmail.com>
2017-05-13 21:21:05 -0700

    fixed missing input

--- src/read_input.c
+++ src/read_input.c
@@ -6,7 +6,7 @@
 /*   By: irhett <irhett@student.42.us.org>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/05/13 19:30:32 by irhett            #+#    #+#             */
-/*   Updated: 2017/05/13 20:57:53 by irhett           ###   ########.fr       */
+/*   Updated: 2017/05/13 21:20:53 by irhett           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,6 @@ int		read_input(t_table *table, int is_query)
 	if (BUFF_SIZE <= 0 || !table)
 		return (-1);
 	if (is_query)
-		return (populate_table(table));
-	return (run_queries(table));
+		return (populate_table(table, &excess));
+	return (run_queries(table, &excess));
 }
